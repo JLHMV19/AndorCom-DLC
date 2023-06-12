5d148162f7d2d12843ed9b191ef1fd8a26ccb926
JeftePlayGame <2102195@utrivieramaya.edu.mx>
2023-06-12 14:06:42 -0500

    a

--- ANDORMED-APP/src/app/app-routing.module.ts
+++ ANDORMED-APP/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component : LoginPageComponent },
+  { path: 'start', component : StartPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ANDORMED-APP/src/app/app.module.ts
+++ ANDORMED-APP/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { BarrioscomponentComponent } from './barrioscomponent/barrioscomponent.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    BarrioscomponentComponent
+    BarrioscomponentComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    StartPageComponent
+    
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
